fa53cb898f72c7bba25abe5176fa1f9cde2decdf
youen plusquellec <youenetleo@gmail.com>
2021-09-29 15:59:16 +0200

    :white_check_mark: Add more tests

--- Assets/Modules/Tiles/Scripts/TilesManager.cs
+++ Assets/Modules/Tiles/Scripts/TilesManager.cs
@@ -6,28 +6,38 @@ namespace Aloha
 {
     public class TilesManager : Singleton<TilesManager>
     {
-        [SerializeField] private GameObject[] tilePrefabs;
-        [SerializeField] private int numberOfTiles;
+        [HideInInspector] public List<GameObject> activeTiles = new List<GameObject>();
+        public GameObject tilesContainer;
+        public bool gameIsStarted;
+        public int numberOfTiles; // min : 2
         public float tileSpeed = 10;
         public float tileSize = 5;
 
-        [HideInInspector] public List<GameObject> activeTiles = new List<GameObject>();
-        private GameObject tilesContainer;
+        [SerializeField] private GameObject[] tilePrefabs;
 
         // Start is called before the first frame update
         void Start()
         {
+            gameIsStarted = false;
             tilesContainer = new GameObject("TilesContainer");
-
-            for (int position = 0; position < numberOfTiles; position++)
-            {
-                SpawnTileAt(Random.Range(0, tilePrefabs.Length), position);
-            }
         }
 
         // Update is called once per frame
         void Update()
         {
+            // Start the game
+            if (!gameIsStarted && Input.GetKeyDown(KeyCode.Space) )
+            {
+                StartGame();
+            }
+
+            // Stop the game
+            if (gameIsStarted && Input.GetKeyDown(KeyCode.Escape))
+            {
+                StopGame();
+            }
+
+            // Delete a tile and replace it by a new one
             if ( activeTiles.Count != 0 && activeTiles[0].transform.position.z + tileSize < 0)
             {
                 DeleteFirstTile();
@@ -35,8 +45,38 @@ namespace Aloha
             }
         }
 
+        public void StartGame()
+        {
+            if (gameIsStarted)
+                return;
+
+            gameIsStarted = true;
+            for (int position = 0; position < numberOfTiles; position++)
+            {
+                SpawnTileAt(Random.Range(0, tilePrefabs.Length), position);
+            }
+        }
+
+        public void StopGame()
+        {
+            if (!gameIsStarted)
+                return;
+
+            gameIsStarted = false;
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                DeleteFirstTile();
+            }
+        }
+
         public void SpawnTileToQueue(int tileIndex)
         {
+            if (activeTiles.Count == 0)
+            {
+                SpawnTileAt(tileIndex, 0);
+                return;
+            }
+
             GameObject tile = Instantiate(tilePrefabs[tileIndex], transform.forward * (activeTiles[activeTiles.Count - 1].transform.position.z + tileSize), transform.rotation, tilesContainer.transform);
             activeTiles.Add(tile);
         }
@@ -52,6 +92,5 @@ namespace Aloha
             Destroy(activeTiles[0]);
             activeTiles.RemoveAt(0);
         }
-
     }
 }
--- Assets/Tests/PlayMode/Tiles/TileTest.cs
+++ Assets/Tests/PlayMode/Tiles/TileTest.cs
@@ -9,39 +9,83 @@ namespace Aloha.Test
 {
     public class TileTest
     {
-        // A Test behaves as an ordinary method
+
+        // This Test checks if tiles are moving on Z- axis
         [UnityTest]
         public IEnumerator TileMoveForward()
         {
             GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GlobalManager"));
             TilesManager tilesManager = TilesManager.Instance;
-
             yield return null;
 
             tilesManager.SpawnTileAt(0, 0);
-            GameObject tile = tilesManager.activeTiles[0];
-
-            float initialZPos = tile.transform.position.z;
-
             yield return null;
+
+            GameObject tile = tilesManager.activeTiles[0];
+            float initialZPos = tile.transform.position.z;
+            yield return new WaitForFixedUpdate();
 
             Assert.Less(tile.transform.position.z, initialZPos);
 
             Object.Destroy(tile);
+            Object.Destroy(tilesManager);
+            Object.Destroy(manager);
+            yield return null;
+        }
+
+        // This test checks that a new tile appears and the first one is destroy when tiles move
+        [UnityTest]
+        public IEnumerator TileAutomaticallyAppearsAndDestroyed()
+        {
+            GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GlobalManager"));
+            TilesManager tilesManager = TilesManager.Instance;
+            yield return null;
+
+            tilesManager.StartGame();
+            yield return null;
+
+            GameObject firstTile = tilesManager.activeTiles[0];
+            GameObject lastTile = tilesManager.activeTiles[tilesManager.activeTiles.Count - 1];
+            yield return new WaitForSeconds( tilesManager.tileSize / tilesManager.tileSpeed );
+
+            if (firstTile) Assert.IsFalse(true, "Is first tile deleted ?");
+            Assert.AreNotSame(lastTile, tilesManager.activeTiles[tilesManager.activeTiles.Count - 1], "Do tiles spawn ?");
+
+            Object.Destroy(lastTile);
+            tilesManager.StopGame();
+            yield return null;
+
+            Object.Destroy(tilesManager);
+            Object.Destroy(manager);
+            yield return null;
 
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        /*[UnityTest]
-        public IEnumerator TileAutomaticallyAppears()
+        // This Test checks if the game is correctly instanced and destroyed when it's start and stop
+        [UnityTest]
+        public IEnumerator GameStartAndStop()
         {
-            //GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TilesManager"));
-            //GameObject go = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Assets/Modules/Tiles/Prefabs/BasicTile"));
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
+            GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GlobalManager"));
+            TilesManager tilesManager = TilesManager.Instance;
             yield return null;
-        }*/
+
+            tilesManager.StartGame();
+            yield return null;
+
+            Assert.IsTrue(tilesManager.gameIsStarted);
+            Assert.AreEqual(tilesManager.tilesContainer.transform.childCount, tilesManager.numberOfTiles);
+
+            tilesManager.StopGame();
+            yield return null;
+
+            Assert.IsFalse(tilesManager.gameIsStarted);
+            Assert.AreEqual(tilesManager.tilesContainer.transform.childCount, 0);
+
+            Object.Destroy(tilesManager);
+            Object.Destroy(manager);
+        }
+
+
     }
 
 }
