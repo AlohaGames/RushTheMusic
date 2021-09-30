a436634e537d4d0167179f622532b3fb35f65a98
youen plusquellec <youenetleo@gmail.com>
2021-09-30 13:05:01 +0200

    :recycle: Refactoring

--- Assets/Modules/Tiles/Scripts/TilesManager.cs
+++ Assets/Modules/Tiles/Scripts/TilesManager.cs
@@ -7,9 +7,9 @@ namespace Aloha
     public class TilesManager : Singleton<TilesManager>
     {
         [HideInInspector] public List<GameObject> activeTiles = new List<GameObject>();
-        public GameObject tilesContainer;
-        public bool gameIsStarted;
-        public int numberOfTiles; // min : 2
+        [HideInInspector] public GameObject tilesContainer;
+        [HideInInspector] public bool gameIsStarted;
+        public int numberOfTiles = 20;
         public float tileSpeed = 10;
         public float tileSize = 5;
 
@@ -38,13 +38,14 @@ namespace Aloha
             }
 
             // Delete a tile and replace it by a new one
-            if ( activeTiles.Count != 0 && activeTiles[0].transform.position.z + tileSize < 0)
+            if (gameIsStarted && activeTiles.Count != 0 && activeTiles[0].transform.position.z + tileSize < 0)
             {
                 DeleteFirstTile();
                 SpawnTileToQueue(Random.Range(0, tilePrefabs.Length));
             }
         }
 
+        // Init elements to make the game start
         public void StartGame()
         {
             if (gameIsStarted)
@@ -57,6 +58,7 @@ namespace Aloha
             }
         }
 
+        // Stop the game and delete elements
         public void StopGame()
         {
             if (!gameIsStarted)
@@ -69,6 +71,7 @@ namespace Aloha
             }
         }
 
+        // Create a tile at the end of the tile list
         public void SpawnTileToQueue(int tileIndex)
         {
             if (activeTiles.Count == 0)
@@ -81,12 +84,15 @@ namespace Aloha
             activeTiles.Add(tile);
         }
 
+        // Create a tile at the position the user wants
+        // (Only if tiles didn't use at all)
         public void SpawnTileAt(int tileIndex, int position)
         {
             GameObject tile = Instantiate(tilePrefabs[tileIndex], transform.forward * (tileSize * position), transform.rotation, tilesContainer.transform);
             activeTiles.Add(tile);
         }
 
+        // Delete the first tile of the list
         private void DeleteFirstTile()
         {
             Destroy(activeTiles[0]);
