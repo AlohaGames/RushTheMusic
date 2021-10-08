e272c40ab0493d7ee61fbae7724b59ef3f676d04
Enoriz <unconfigured@null.spigotmc.org>
2021-10-08 12:12:50 +0200

    Utils not static

--- Assets/Modules/0_Global/Scripts/Utils.cs
+++ Assets/Modules/0_Global/Scripts/Utils.cs
@@ -1,6 +1,6 @@
 using System;
-public static class Utils{
-    public static bool EqualFloat(float a, float b, float epsilon=0.01f){
+public class Utils{
+    public bool EqualFloat(float a, float b, float epsilon=0.01f){
         return Math.Abs(a-b) <= epsilon;
     }
 }
