522c53d9fed0deb6c65df1c74c5e1a68f9bbfa23
Wilfried PEPIN <wilfried.pepin35133@gmail.com>
2021-10-08 16:19:49 +0200

    :fire: fix updates

--- Assets/Modules/menus/Scripts/MenuScript.cs
+++ Assets/Modules/menus/Scripts/MenuScript.cs
@@ -7,27 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public UnityEvent OnHover;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-    public void OnMouseEnter()
-    {
-        Debug.Log("cc");
-        OnHover.Invoke();
-    }
-
 
     public void Play()
     {
@@ -36,7 +15,6 @@ public class MenuScript : MonoBehaviour
 
     public void Quit()
     {
-        Debug.Log("quit");
         Application.Quit();
     }
 }
