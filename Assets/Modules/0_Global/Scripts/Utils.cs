a7fcaa6ae0985c99dbfb39c306a2bf7fe38648e0
Enoriz <unconfigured@null.spigotmc.org>
2021-10-08 12:14:55 +0200

    oupsi EqualFloat

--- Assets/Modules/0_Global/Scripts/Utils.cs
+++ Assets/Modules/0_Global/Scripts/Utils.cs
@@ -1,6 +1,6 @@
 using System;
 public class Utils{
-    public bool EqualFloat(float a, float b, float epsilon=0.01f){
+    public static bool EqualFloat(float a, float b, float epsilon=0.01f){
         return Math.Abs(a-b) <= epsilon;
     }
 }
