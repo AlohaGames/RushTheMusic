bebfb80eada31cfa5a6a4bce4a48c2f7ec26aa4f
Enoriz <unconfigured@null.spigotmc.org>
2021-10-12 11:49:07 +0200

    :construction: Add end game scene to show score details

--- Assets/Modules/Score/Scripts/UIManager.cs
+++ Assets/Modules/Score/Scripts/UIManager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Aloha.Score;
+using Aloha;
 
 namespace Aloha
 {
     public class UIManager : MonoBehaviour
     {
         public Text scoreText;
+
         // Update is called once per frame
         void Update()
         {
             scoreText.text = "Score total: " + ScoreManager.Instance.CalculateTotalScore().ToString();
+            if(!TilesManager.Instance.gameIsStarted){
+                scoreText.text = SceneManager.GetActiveScene().name.ToString();
+            }
         }
     }
 }
--- Assets/Tests/PlayMode/Utils/Utils.cs
+++ Assets/Tests/PlayMode/Utils/Utils.cs
@@ -1,56 +1,62 @@
 using NUnit.Framework;
 
-public class UtilsTest
+namespace Aloha.test
 {
-    [Test]
-    public void EqualFloatTest()
+    public class UtilsTest
     {
-        //True
-        Assert.IsTrue(Utils.EqualFloat(3.2254346f, 3.22f));
+        [Test]
+        public void EqualFloatTest()
+        {
+            //True
+            Assert.IsTrue(Utils.EqualFloat(3.2254346f, 3.22f));
 
-        //False
-        Assert.IsTrue(Utils.EqualFloat(3.219453f, 3.2254655f));
+            //False
+            Assert.IsTrue(Utils.EqualFloat(3.219453f, 3.2254655f));
 
-        //Epsilon True
-        Assert.IsTrue(Utils.EqualFloat(3.213f, 3.213468484f, 0.001f));
+            //Epsilon True
+            Assert.IsTrue(Utils.EqualFloat(3.213f, 3.213468484f, 0.001f));
 
-        //Epsilon False
-        Assert.IsTrue(Utils.EqualFloat(3.2139484548f, 3.21456226685f));
+            //Epsilon False
+            Assert.IsTrue(Utils.EqualFloat(3.2139484548f, 3.21456226685f));
+        }
+
+        [Test]
+        public void InRangeIntTest()
+        {
+            //True positive
+            Assert.AreEqual(0, Utils.InRangeInt(-10, 10, 0));
+
+            //True max positive
+            Assert.AreEqual(10, Utils.InRangeInt(-10, 10, 10));
+
+            //True min negative
+            Assert.AreEqual(-10, Utils.InRangeInt(-10, 10, -10));
+
+            //False above min negative
+            Assert.AreEqual(-10, Utils.InRangeInt(-10, 10, -11));
+
+            //False above max positive
+            Assert.AreEqual(10, Utils.InRangeInt(-10, 10, 11));
+        }
+
+        [Test]
+        public void InRangeFloatTest()
+        {
+            //True positive
+            Assert.AreEqual(0f, Utils.InRangeFloat(-10f, 10f, 0f));
+
+            //True max positive
+            Assert.AreEqual(10f, Utils.InRangeFloat(-10f, 10f, 10f));
+
+            //True min negative
+            Assert.AreEqual(-10f, Utils.InRangeFloat(-10f, 10f, -10f));
+
+            //False above min negative
+            Assert.AreEqual(-10f, Utils.InRangeFloat(-10f, 10f, -11f));
+
+            //False above max positive
+            Assert.AreEqual(10f, Utils.InRangeFloat(-10f, 10f, 11f));
+        }
     }
 
-    [Test]
-    public void InRangeIntTest(){
-        //True positive
-        Assert.AreEqual(0, Utils.InRangeInt(-10, 10, 0));
-
-        //True max positive
-        Assert.AreEqual(10, Utils.InRangeInt(-10, 10, 10));
-
-        //True min negative
-        Assert.AreEqual(-10, Utils.InRangeInt(-10, 10, -10));
-        
-        //False above min negative
-        Assert.AreEqual(-10, Utils.InRangeInt(-10, 10, -11));
-
-        //False above max positive
-        Assert.AreEqual(10, Utils.InRangeInt(-10, 10, 11));
-    }
-
-    [Test]
-    public void InRangeFloatTest(){
-        //True positive
-        Assert.AreEqual(0f, Utils.InRangeFloat(-10f, 10f, 0f));
-
-        //True max positive
-        Assert.AreEqual(10f, Utils.InRangeFloat(-10f, 10f, 10f));
-
-        //True min negative
-        Assert.AreEqual(-10f, Utils.InRangeFloat(-10f, 10f, -10f));
-        
-        //False above min negative
-        Assert.AreEqual(-10f, Utils.InRangeFloat(-10f, 10f, -11f));
-
-        //False above max positive
-        Assert.AreEqual(10f, Utils.InRangeFloat(-10f, 10f, 11f));
-    }
 }
