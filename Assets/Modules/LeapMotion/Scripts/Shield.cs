87ca44da58552cc1ab4883e91ffb9f57901de486
youen <33334707+youenPlusquellec@users.noreply.github.com>
2021-10-18 10:52:18 +0200

    ✨ Add leap motion support for warrior (#11)
    * :sparkles: Add leap support for warrior attack
    
    * :sparkles: Add leap support for warrior defense
    
    * :sparkles: Attack and Defense according to warrior
    
    * :sparkles: Add bumping feature
    
    * :white_check_mark: Add unit tests for leap
    
    * 💚 Resolve CI-CD failure
    
    * :recycle: Refactor entities and tests
    
    Co-authored-by: Thomas Quintrand <thomas.quintrand@outlook,fr>
    Co-authored-by: Dorian <d.bricaud99@gmail.com>

---
+++ Assets/Modules/LeapMotion/Scripts/Shield.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Aloha.Hero;
+
+namespace Aloha
+{
+    public class Shield : MonoBehaviour
+    {
+        public Warrior warrior;
+        private Vector3 presPos;
+        private Vector3 newPos;
+        public float speed;
+
+        private void Start()
+        {
+            presPos = transform.position;
+            newPos = transform.position;
+        }
+
+        private void Update()
+        {
+            newPos = transform.position;
+            speed = (newPos - presPos).magnitude * 100;
+            presPos = newPos;
+        }
+
+        // If the Shield touch an Object
+        public void OnTriggerEnter(Collider collider)
+        {
+            if (collider.tag == "Enemy")
+            {
+                // Change minimum speed if actual speed is to low
+                if (speed < 1) speed = 1f;
+                warrior.BumpEntity(collider.GetComponent<Entity>(),speed);
+            }
+        }
+    }
+}
