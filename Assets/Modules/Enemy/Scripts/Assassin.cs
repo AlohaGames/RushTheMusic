18f226d471bb192ee125a8c00ea789e8e2207b84
youen plusquellec <youenetleo@gmail.com>
2021-10-19 16:44:18 +0200

    :bento: Add Assassin's prefab

---
+++ Assets/Modules/Enemy/Scripts/Assassin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Aloha.EntityStats;
+
+namespace Aloha
+{
+    public class Assassin : Enemy<AssassinStats> 
+    {
+        
+    }
+}
