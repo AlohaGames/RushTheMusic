a1671fad7536d785ab55cf7b050de0e98a6a5cd4
Enora <enora@eno.com>
2021-10-19 13:57:22 +0200

    :white_check_mark: Add test for UI score

--- Assets/Tests/PlayMode/Score/UIScoreTest.cs
+++ Assets/Tests/PlayMode/Score/UIScoreTest.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.TestTools;
 using NUnit.Framework;
+using System.Collections;
 using Aloha.Score;
 
 namespace Aloha.Test
@@ -7,14 +9,48 @@ namespace Aloha.Test
     public class UIScoreTest
     {
         [Test]
-        public void UIScoreInGame(){
+        public void UIScoreInGameTest()
+        {
             GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GlobalManager"));
             ScoreManager scoreManager = ScoreManager.Instance;
 
             Assert.IsTrue(scoreManager != null);
 
+            Debug.Log(scoreManager);
+
             Assert.IsFalse(scoreManager.GetComponent<UIManager>().canvasUIScoreLevel.activeSelf);
             Assert.IsTrue(scoreManager.GetComponent<UIManager>().scoreText.IsActive());
+
+            GameObject.DestroyImmediate(manager);
+        }
+
+        [UnityTest]
+        public IEnumerator UIScoreEndLevelTest()
+        {
+            GameObject manager = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/GlobalManager"));
+            ScoreManager scoreManager = ScoreManager.Instance;
+            GameManager gameManager = GameManager.Instance;
+
+            Assert.IsTrue(scoreManager != null);
+
+            yield return null;
+
+            Debug.Log(gameManager.isPlaying);
+
+            gameManager.StopGame();
+
+            Debug.Log(gameManager.isPlaying);
+
+            yield return null;
+
+            Assert.IsTrue(scoreManager.GetComponent<UIManager>().canvasUIScoreLevel.activeSelf);
+            Assert.IsFalse(scoreManager.GetComponent<UIManager>().scoreText.IsActive());
+
+            Debug.Log(scoreManager.GetComponent<UIManager>().canvasUIScoreLevel.activeSelf);
+
+            GameObject.Destroy(manager);
+
+            yield return null;
         }
     }
 }
