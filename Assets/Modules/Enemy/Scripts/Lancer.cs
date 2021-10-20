8377f7c6328b4343f70cfadfb610990df9a4c9a3
youen plusquellec <youenetleo@gmail.com>
2021-10-20 15:30:12 +0200

    :bento: Add lancer prefab

---
+++ Assets/Modules/Enemy/Scripts/Lancer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Aloha.EntityStats;
+
+namespace Aloha
+{
+    public class Lancer : Enemy<LancerStats> { 
+    
+    }
+}
