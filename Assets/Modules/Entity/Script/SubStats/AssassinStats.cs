71eb0a3afd3633b1a405ca5b81dce78c9996a9dd
youen <33334707+youenPlusquellec@users.noreply.github.com>
2021-11-10 11:35:47 +0100

    ✨  Add assassin prefab (#25)
    * :bento: Add Assassin's prefab
    
    * :construction: WIP Update assassin's prefab and scripts
    
    * :bento: Add lancer prefab
    
    * :sparkles: Add enemy autonomy and AI
    
    * :construction: Update scene
    
    * :sparkles: Add NoAI option
    
    * :sparkles: Add assassin's support in manager
    
    * Add AI for Assassin
    
    Co-authored-by: Dorian <d.bricaud99@gmail.com>
    Co-authored-by: Thomas Quintrand <thomas.quintrand@outlook,fr>
    Co-authored-by: Dorian Bricaud <33903819+DoBrcd@users.noreply.github.com>

---
+++ Assets/Modules/Entity/Script/SubStats/AssassinStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Aloha.EntityStats
+{
+    [CreateAssetMenu(fileName = "AssassinStats", menuName = "Stats/Enemy/Assassin", order = 1)]
+    public class AssassinStats : EnemyStats 
+    {
+        
+    }
+}
