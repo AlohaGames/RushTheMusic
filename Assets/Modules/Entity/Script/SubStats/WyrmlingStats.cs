b4a22e403644d9fda2535a0f3927a742ca3344d7
youen plusquellec <youenetleo@gmail.com>
2021-11-10 11:27:19 +0100

    :bento: Add wyrmling prefab

---
+++ Assets/Modules/Entity/Script/SubStats/WyrmlingStats.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace Aloha.EntityStats
+{
+    [CreateAssetMenu(fileName = "WyrmlingStats", menuName = "Stats/Enemy/Wyrmling", order = 1)]
+    public class WyrmlingStats : EnemyStats { }
+}
