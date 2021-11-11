ba36aaf50a7aabce4d099389c6629eba82790738
Xenilov <tristan.couturier1998@gmail.com>
2021-11-11 16:51:01 +0100

    :sparkles: Add assassin, wyrmling and more

---
+++ Assets/Modules/SideEnvironment/Scripts/Rock.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aloha
+{
+    public class Rock : SideEnvironment
+    {
+        public override void Initialize()
+        {
+            height = 1;
+        }
+    }
+
+}
