883062f5e345698c94d57e4e40f0e3714264e6c9
Youen Plusquellec <youenetleo@gmail.com>
2021-11-18 23:19:32 +0100

    :sparkles: Add limits to camera rotation

--- Assets/Modules/Hero/Scripts/ControlManager.cs
+++ Assets/Modules/Hero/Scripts/ControlManager.cs
@@ -4,56 +4,68 @@ using UnityEngine;
 
 namespace Aloha
 {
-public class ControlManager : MonoBehaviour
-{
-    //TODO Set mode in gameManager
-    private bool leapMode = true;
-
-    // Start is called before the first frame update
-    void Start()
+    public class ControlManager : MonoBehaviour
     {
-        leapMode = false;
-    }
+        //TODO Set mode in gameManager
+        private bool leapMode = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (!leapMode && GameManager.Instance.GetIsPlaying())
+        // Start is called before the first frame update
+        void Start()
         {
-            float ry = Input.GetAxis("Mouse Y");
-            float rx = Input.GetAxis("Mouse X");
-            transform.RotateAround(this.transform.parent.transform.position, this.transform.parent.transform.up, rx);
-            transform.RotateAround(this.transform.parent.transform.position, this.transform.parent.transform.right, -ry);
-            CheckInputs();
+            leapMode = false;
         }
-    }
 
-    void CheckInputs()
-    {
-        if (Input.GetKeyDown(InputBinding.Instance.attack))
+        // Update is called once per frame
+        void Update()
         {
-            PrepareAttack();
+            if (!leapMode && GameManager.Instance.GetIsPlaying())
+            {
+                // Get camera rotation
+                float ry = Input.GetAxis("Mouse Y");
+                float rx = Input.GetAxis("Mouse X");
+
+                // Rotate hands on y axis if it's in the camera angle
+                if ((transform.rotation.y > -0.5f && rx < 0) || (transform.rotation.y < 0.5f && rx > 0))
+                {
+                    transform.RotateAround(this.transform.parent.transform.position, this.transform.parent.transform.up, rx);
+                }
+
+                // Rotate hands on x axis if it's in the camera angle
+                if ((transform.rotation.x > -0.25f && ry > 0) || (transform.rotation.x < 0.1f && ry < 0))
+                {
+                    transform.RotateAround(this.transform.parent.transform.position, this.transform.parent.transform.right, -ry);
+                }
+                
+                CheckInputs();
+            }
         }
-        else if (Input.GetKeyUp(InputBinding.Instance.attack))
+
+        void CheckInputs()
         {
-            ReleaseAttack();
+            if (Input.GetKeyDown(InputBinding.Instance.attack))
+            {
+                PrepareAttack();
+            }
+            else if (Input.GetKeyUp(InputBinding.Instance.attack))
+            {
+                ReleaseAttack();
+            }
+            else if (Input.GetKeyDown(InputBinding.Instance.defense))
+            {
+                PrepareDefense();
+            }
+            else if (Input.GetKeyUp(InputBinding.Instance.defense))
+            {
+                ReleaseDefense();
+            }
         }
-        else if (Input.GetKeyDown(InputBinding.Instance.defense))
-        {
-            PrepareDefense();
-        }
-        else if (Input.GetKeyUp(InputBinding.Instance.defense))
-        {
-            ReleaseDefense();
-        }
-    }
 
-    protected virtual void PrepareAttack() { }
+        protected virtual void PrepareAttack() { }
 
-    protected virtual void ReleaseAttack() { }
+        protected virtual void ReleaseAttack() { }
 
-    protected virtual void PrepareDefense() { }
+        protected virtual void PrepareDefense() { }
 
-    protected virtual void ReleaseDefense() { }
+        protected virtual void ReleaseDefense() { }
     }
 }
