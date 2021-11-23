fddd1aad42b84ff450fe4a7962a82bdb12874546
wilfried7 <73899903+wilfried7@users.noreply.github.com>
2021-11-23 21:02:07 +0100

    🎨 Add UI for inventory (#37)
    * :construct: end of inventory, check for linter
    
    * :construct: some changes
    
    * :green_heart: Resolve CI Error
    
    * :construction: add doc
    
    * some changes
    
    * :construction: encore & encore
    
    * encore & encore & encore
    
    * :memo: Update documentation
    
    * 🐛 Fix UI inventory (#46)
    
    * :art: Trying to create itemContainer prefa
    
    * :sparkles: Fix inventory UI bug
    
    * :construction: Add some scripts
    
    * :construction: wip Add small boxes support
    
    * :spakles: finish inventory ui
    
    * :memo: Remove docs which do not work
    
    Co-authored-by: Enoriz <enora.morizur@gmail.com>
    
    * :memo: Update docs
    
    * :sparkles: Use of items with keyboard and leap
    
    Co-authored-by: Dorian Bricaud <33903819+DoBrcd@users.noreply.github.com>
    Co-authored-by: Dorian <d.bricaud99@gmail.com>
    Co-authored-by: Youen Plusquellec <youenetleo@gmail.com>
    Co-authored-by: youen <33334707+youenPlusquellec@users.noreply.github.com>
    Co-authored-by: Enoriz <enora.morizur@gmail.com>

---
+++ Assets/Modules/Inventory/Scripts/InventoryManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Aloha
+{
+    /// <summary>
+    /// This class manages the inventory
+    /// </summary>
+    public class InventoryManager  : Singleton<InventoryManager>
+    {
+        private Queue<Item> items = new Queue<Item>();
+        private int maxItem = 5;
+
+        /// <summary>
+        /// Add an item to the inventory, if the inventory is full, the new item will be dropped
+        /// <example> Example(s):
+        /// <code>
+        ///     AddItem(item);
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddItem(Item item)
+        {
+            if(this.items.Count < this.maxItem)
+            {
+                this.items.Enqueue(item);
+            }
+            UIManager.Instance.UIInventory.UpdateInventoryUI();
+        }
+
+        /// <summary>
+        /// Use the first item of the queue and delete it after
+        /// <example> Example(s):
+        /// <code>
+        ///     AddItem(item);
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void UseItem()
+        {
+            if(this.items.Count > 0)
+            {
+                Item item = this.items.Dequeue();
+                item.Effect();
+            }
+            UIManager.Instance.UIInventory.UpdateInventoryUI();
+        }
+
+        /// <summary>
+        /// Get all the items of our inventory. WARNING : it's not a copy, it's the instance of the items
+        /// <example> Example(s):
+        /// <code>
+        ///     Inventory inventory = new Inventory();
+        ///     Queue(item) items = inventory.GetItems();
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <returns>
+        /// A queue with all the items in the inventory
+        /// </returns>
+        public Queue<Item> GetItems()
+        {
+            return this.items;
+        }
+
+        /// <summary>
+        /// Get the max size of the inventory
+        /// <example> Example(s):
+        /// <code>
+        ///     Inventory inventory = new Inventory();
+        ///     int maxItems = inventory.GetMaxItems();
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <returns>
+        /// An integer with the value of the max size of the inventory.
+        /// </returns>
+        public int GetMaxItems()
+        {
+            return this.maxItem;
+        }
+    }
+}
