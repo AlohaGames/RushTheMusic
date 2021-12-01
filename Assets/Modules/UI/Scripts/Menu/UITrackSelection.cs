4399e19e1eede364d19179029dffd36b687fe744
Enoriz <unconfigured@null.spigotmc.org>
2021-12-01 21:55:33 +0100

    :art: Set menu to prefab and add new Game scene, TheGame

--- Assets/Modules/UI/Scripts/Menu/UITrackSelection.cs
+++ Assets/Modules/UI/Scripts/Menu/UITrackSelection.cs
@@ -1,11 +1,52 @@
 using UnityEngine;
+using System.Collections;
 
 namespace Aloha
 {
-    public class UITrackSelection
+    // TODO
+    /// <summary>
+    /// Manage the track selection UI
+    /// </summary>
+    public class UITrackSelection : MonoBehaviour
     {
-        public GameObject TrackElement;
+        [SerializeField]
+        private GameObject trackElementPrefab;
 
+        public GameObject TrackSelectionUI;
 
+        /// <summary>
+        /// Get the list of tracks 
+        /// <example> Example(s):
+        /// <code>
+        ///     GetTrackList()
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void GetTrackList()
+        {
+            // TODO
+        }
+
+        /// <summary>
+        /// Display track list UI
+        /// <example> Example(s):
+        /// <code>
+        ///     GetTrackList()
+        /// </code>
+        /// </example>
+        /// </summary>
+        public void ShowTrackList()
+        {
+            int trackNumber = 3;
+            
+            if(trackNumber >= 1)
+            {
+                for(int i = 0; i < trackNumber - 1; i++)
+                {
+                    GameObject trackElement = Instantiate(trackElementPrefab);
+                    trackElement.transform.SetParent(TrackSelectionUI.transform);
+                }
+            }
+        }
     }
 }
