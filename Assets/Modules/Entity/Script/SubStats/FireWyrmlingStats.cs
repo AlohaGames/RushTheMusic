de6c36ed8e9886948c5ef255098900751116d7cc
youen plusquellec <youenetleo@gmail.com>
2021-12-28 16:34:14 +0100

    :memo: Add some docs

--- Assets/Modules/Entity/Script/SubStats/FireWyrmlingStats.cs
+++ Assets/Modules/Entity/Script/SubStats/FireWyrmlingStats.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Aloha.EntityStats
 {
     /// <summary>
-    /// TODO
+    /// Stats for the fire wyrmling
     /// </summary>
     [CreateAssetMenu(fileName = "FireWyrmlingStats", menuName = "Stats/Enemy/FireWyrmling", order = 1)]
     public class FireWyrmlingStats : WyrmlingStats { }
--- Assets/Modules/Entity/Script/SubStats/IceWyrmlingStats.cs
+++ Assets/Modules/Entity/Script/SubStats/IceWyrmlingStats.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Aloha.EntityStats
 {
     /// <summary>
-    /// TODO
+    /// Stats for the ice wyrmling
     /// </summary>
     [CreateAssetMenu(fileName = "IceWyrmlingStats", menuName = "Stats/Enemy/IceWyrmling", order = 1)]
     public class IceWyrmlingStats : WyrmlingStats { }
