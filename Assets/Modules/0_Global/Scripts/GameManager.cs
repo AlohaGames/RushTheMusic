079d6e91331607edda5a4c5b95353c535f145bd7
Enoriz <unconfigured@null.spigotmc.org>
2022-01-11 21:19:16 +0100

    :bug: Fix paused during victory screen

--- Assets/Modules/0_Global/Scripts/GameManager.cs
+++ Assets/Modules/0_Global/Scripts/GameManager.cs
@@ -12,6 +12,7 @@ namespace Aloha
     {
         private bool isGamePaused = false;
         private Hero hero;
+        private bool isGameFinished = false;
 
         [SerializeField]
         private string defaultLevel = "";
@@ -71,6 +72,7 @@ namespace Aloha
             UnFreeze();
             IsPlaying = true;
             isGamePaused = false;
+            isGameFinished = false;
             GlobalEvent.LevelStart.Invoke();
         }
 
@@ -105,6 +107,7 @@ namespace Aloha
         public void FinishLevel()
         {
             Freeze();
+            isGameFinished = true;
             ContainerManager.Instance.ClearContainers(
                 new[] { ContainerTypes.Enemy, ContainerTypes.Projectile }
             );
@@ -140,6 +143,22 @@ namespace Aloha
         public bool IsGamePaused()
         {
             return this.isGamePaused;
+        }
+
+        /// <summary>
+        /// Will return if the game is finished or not
+        /// <example> Example(s):
+        /// <code>
+        ///     GameManager.Instance.IsGameFinished()
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <returns>
+        /// a boolean if the game is finished or not
+        /// </returns>
+        public bool IsGameFinished()
+        {
+            return this.isGameFinished;
         }
 
         /// <summary>
@@ -241,7 +260,7 @@ namespace Aloha
         {
             if (Input.GetKeyDown(InputBinding.Instance.Pause))
             {
-                if (IsPlaying)
+                if (IsPlaying && !isGameFinished)
                 {
                     if (isGamePaused)
                         ResumeGame();
