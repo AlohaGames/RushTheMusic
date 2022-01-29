2872df57abf90b9ff053481b14701f586aec732d
Youen Plusquellec <youenetleo@gmail.com>
2022-01-29 02:18:12 +0100

    :bug: Repair stopping coroutines

--- Assets/Modules/UI/Scripts/Menu/ImageAnimation.cs
+++ Assets/Modules/UI/Scripts/Menu/ImageAnimation.cs
@@ -25,6 +25,17 @@ namespace Aloha.UI
 		void Awake()
 		{
 			image = GetComponent<Image>();
+		}
+
+		/// <summary>
+		/// Is called when the script instance is active.
+		/// </summary>
+		void OnEnable()
+		{
+			// Set to default value
+			index = 0;
+
+			// Start the coroutine
 			StartCoroutine(Animate(this.Delay));
 		}
 
@@ -33,17 +44,33 @@ namespace Aloha.UI
 		/// </summary>
 		private IEnumerator Animate(float delay)
 		{
+			Debug.Log(">> Animate");
 			while (true)
-            {
+			{
+				Debug.Log(">> Animate >> while");
 				image.sprite = Sprites[index];
 				index++;
+				Debug.Log(">> Animate >> index++ ? "+index);
+				Debug.Log(">> Animate >> Sprites.Length ? " + Sprites.Length);
 				if (index >= Sprites.Length)
 				{
+					Debug.Log(">> Animate >> if");
 					if (Loop) index = 0;
+					Debug.Log(">> Animate >> loop");
+					Debug.Log(">> Animate >> destroyOnEnd ? "+DestroyOnEnd);
 					if (DestroyOnEnd) Destroy(gameObject);
+					Debug.Log(">> Animate >> destroy");
 				}
+				Debug.Log(">> Animate >> Wait for "+delay);
 				yield return new WaitForSeconds(delay);
+				Debug.Log(">> Animate >> after waiting");
+				yield return null;
 			}
 		}
-	}
+
+        private void OnDestroy()
+        {
+			Debug.Log("DESTROY");
+        }
+    }
 }
