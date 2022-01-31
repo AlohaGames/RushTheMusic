c0f60b9a9f36d53ced598862783c2fe45189a637
youen <33334707+youenPlusquellec@users.noreply.github.com>
2022-01-31 13:53:44 +0100

    🐛 Fix wall bug (#107)
    * :bug: Fix wall bug
    
    * :pencil2: Fix method typo

--- Assets/Modules/Enemy/Scripts/Wall.cs
+++ Assets/Modules/Enemy/Scripts/Wall.cs
@@ -23,7 +23,7 @@ namespace Aloha
             anim = GetComponent<Animator>();
             lastTileSpeed = 0;
             isTilesStopped = false;
-            this.NearHeroTrigger.AddListener(runAndStopTiles);
+            this.NearHeroTrigger.AddListener(RunAndStopTiles);
         }
 
         /// <summary>
@@ -49,7 +49,7 @@ namespace Aloha
         /// </code>
         /// </example>
         /// </summary>
-        private void runAndStopTiles()
+        private void RunAndStopTiles()
         {
             if (!isTilesStopped)
             {
@@ -72,8 +72,16 @@ namespace Aloha
         public override void Die()
         {
             anim.SetTrigger("isDead");
-            if (isTilesStopped) runAndStopTiles();
+            if (isTilesStopped) RunAndStopTiles();
             base.Die();
+        }
+
+        /// <summary>
+        /// Security if the game is loose when the game is stopped
+        /// </summary>
+        private void OnDestroy()
+        {
+            if (isTilesStopped) RunAndStopTiles();
         }
     }
 }
